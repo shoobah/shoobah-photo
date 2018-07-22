0e876bb2a9c87b34d146a62da6c382b18d678080
shoobah <shoobah@gmail.com>
2018-07-22 13:27:01 +0200

    Logo and header

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <Logo />
+    </div>
+</template>
+
+<script>
+import Logo from "./Logo.vue";
+
+export default {
+  name: "Header",
+  components: {
+    Logo
+  }
+};
+</script>
+
+
